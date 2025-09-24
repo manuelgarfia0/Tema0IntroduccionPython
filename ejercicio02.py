b0ac9bb31189f978cd3d030ef69d539231dd9794
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-09-24 10:13:55 +0200

    Falta ejercicio 12

--- ejercicio02.py
+++ ejercicio02.py
@@ -4,7 +4,7 @@ num2 = int(input("Introduce el segundo número: "))
 # Comprobar cuál de los dos números es mayor y mostrarlos en orden ascendente
 if num1 < num2:
     print("Los números ordenados son: " + str(num1), str(num2))
-elif num1 = num2:
+elif num1 == num2:
     print("Los números son iguales: " + str(num1), str(num2))
 else:
     print("Los números ordenados son: " + str(num1), str(num2))
