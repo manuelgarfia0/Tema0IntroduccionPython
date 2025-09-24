2f7dea1910994d934f48fc4313afa6dbaef06367
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-09-24 08:24:04 +0200

    Empezado los ejercicios

---
+++ ejercicio01.py
@@ -0,0 +1,8 @@
+# Pedir al usuario que introduzca un número
+print("Introduce un número: ")
+num = int(input())
+# Comprobar si el número es par o impar
+if num % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
---
+++ ejercicio02.py
@@ -0,0 +1,8 @@
+# 
+num1 = int(input("Introduce el primer número: "))
+num2 = int(input("Introduce el segundo número: "))
+
+if num1 < num2:
+    print("Los números ordenados son: " + str(num1), str(num2))
+else:
+    print("Los números ordenados son: " + str(num1), str(num2))
